ded806a430ccb3e246afdf36e8658d5d520c7f4b
rokon-just <rokon.softworldit@gmail.com>
2025-10-21 13:17:34 +0600

    Tag image upload file Issue Solved Done

--- src/app/modules/tags/tags.routes.ts
+++ src/app/modules/tags/tags.routes.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import { multerUpload } from "../../config/multer.config";
+import validateRequest from "../../middlewares/validateRequest";
 import { tagControllers } from "./tags.controllers";
+import { createTagZodSchema } from "./tags.validations";
 
 const router = express.Router();
 
@@ -7,9 +10,25 @@ router.get("/", tagControllers.getAllTags);
 
 router.get("/:id", tagControllers.getSingleTag);
 
-router.post("/create-tag", tagControllers.createTag);
+router.post(
+  "/create-tag",
+  multerUpload.fields([
+    { name: "imageFile", maxCount: 1 },
+    { name: "iconFile", maxCount: 1 },
+  ]),
+  validateRequest(createTagZodSchema),
+  tagControllers.createTag
+);
+
+router.patch(
+  "/update-tag/:id",
+  multerUpload.fields([
+    { name: "imageFile", maxCount: 1 },
+    { name: "iconFile", maxCount: 1 },
+  ]),
+  tagControllers.updateTag
+);
 
 router.delete("/delete-tag/:id", tagControllers.deleteTag);
-router.patch("/update-tag/:id", tagControllers.updateTag);
 
 export const TagRoutes = router;
