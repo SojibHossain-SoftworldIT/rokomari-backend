0e599636cbb2a319fbc73f7042713f6e95099f28
ismail-softworldit <ismail.softworldit@gmail.com>
2025-10-30 15:41:45 +0600

    order status change

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { orderControllers } from "./order.controller";
-import { createOrderZodSchema } from "./order.validation";
+import {
+  changeOrderStatusZodSchema,
+  createOrderZodSchema,
+} from "./order.validation";
 
 const router = express.Router();
 
 router.get("/", orderControllers.getAllOrder);
 
 router.get("/order-summary", orderControllers.getOrderSummary);
+router.get("/order-range-summary", orderControllers.getOrderRangeSummary);
 
 router.get("/my-order/:id", orderControllers.getMyOrders);
 
@@ -19,6 +23,12 @@ router.post(
   "/create-order",
   validateRequest(createOrderZodSchema),
   orderControllers.createOrder
+);
+
+router.patch(
+  "/change-status/:orderId",
+  validateRequest(changeOrderStatusZodSchema), // optional
+  orderControllers.changeOrderStatus
 );
 
 router.patch("/:id", orderControllers.updateOrder);
